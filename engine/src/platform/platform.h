d12ab3d2dc19dfa2fc13f0738f91511236c25dd6
wake-code <87005812+wake-code@users.noreply.github.com>
2021-10-17 00:03:41 +0300

    Reorganize

--- engine/src/platform/platform.h
+++ engine/src/platform/platform.h
@@ -7,9 +7,10 @@ typedef struct platform_state {
 } platform_state;
 
 b8 platform_startup(platform_state* plat_state, const char* application_name, i32 x, i32 y, i32 width, i32 height);
-b8 platform_pump_messages(platform_state* plat_state);
 
 void platform_shutdown(platform_state* plat_state);
+
+b8 platform_pump_messages(platform_state* plat_state);
 
 void* platform_allocate(u64 size, b8 aligned);
 void platform_free(void* block, b8 aligned);
