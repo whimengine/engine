31bd6dd280337b6586b7bcdd3d27b170e07f144e
wake-code <pro-uno-player@protonmail.com>
2021-10-16 11:28:43 -0400

    Basic logging implemented

---
+++ engine/src/core/logger.c
@@ -0,0 +1,35 @@
+#include "logger.h"
+
+// TODO: Temporary
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+b8 initialize_logging() {
+    // TODO: Create log file
+    return TRUE;
+}
+
+void shutdown_logging() {
+    // TODO: Cleanup logging/any queued entries
+}
+
+WAPI void log_output(log_level level, const char* message, ...) {
+    const char* level_strings[6] = {"[FATAL] ", "[ERROR] ", "[WARN] ", "[INFO] ", "[DEBUG] ", "[TRACE] "};
+    b8 is_error = level < 2;
+
+    // FIXME: Find a way around using dynamic memory allocations because it's slow
+    char out_message[32000];
+    
+    memset(out_message, 0, sizeof(out_message));
+
+    // Format original message
+    __builtin_va_list arg_ptr;
+
+    va_start(arg_ptr, message);
+    vsnprintf(out_message, 32000, message, arg_ptr);
+    va_end(arg_ptr);
+
+    sprintf(out_message, "%s%s\n", level_strings[level], out_message);
+    printf("%s", out_message); // TODO: Platform specific output
+}
---
+++ engine/src/core/logger.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "defines.h"
+
+#define LOG_WARN_ENABLED 1
+#define LOG_INFO_ENABLED 1
+#define LOG_DEBUG_ENABLED 1
+#define LOG_TRACE_ENABLED 1
+
+#if WRELEASE == 1
+#define LOG_DEBUG_ENABLED 0
+#define LOG_TRACE_ENABLED 0
+#endif
+
+typedef enum log_level {
+    LOG_LEVEL_FATAL = 0,
+    LOG_LEVEL_ERROR = 1,
+    LOG_LEVEL_WARN  = 2,
+    LOG_LEVEL_INFO  = 3,
+    LOG_LEVEL_DEBUG = 4,
+    LOG_LEVEL_TRACE = 5
+} log_level;
+
+b8 initialize_logging();
+void shutdown_logging();
+
+WAPI void log_output(log_level level, const char* message, ...);
+
+#define WFATAL(message, ...) log_output(LOG_LEVEL_FATAL, message, ##__VA_ARGS__);
+
+#ifndef WERROR
+#define WERROR(message, ...) log_output(LOG_LEVEL_ERROR, message, ##__VA_ARGS__);
+#endif
+
+#if LOG_WARN_ENABLED == 1
+#define WWARN(message, ...) log_output(LOG_LEVEL_WARN, message, ##__VA_ARGS__);
+#else
+#define WWARN(message, ...)
+#endif
+
+#if LOG_INFO_ENABLED == 1
+#define WINFO(message, ...) log_output(LOG_LEVEL_INFO, message, ##__VA_ARGS__);
+#else
+#define WINFO(message, ...)
+#endif
+
+#if LOG_DEBUG_ENABLED == 1
+#define WDEBUG(message, ...) log_output(LOG_LEVEL_DEBUG, message, ##__VA_ARGS__);
+#else
+#define WDEBUG(message, ...)
+#endif
+
+#if LOG_TRACE_ENABLED == 1
+#define WTRACE(message, ...) log_output(LOG_LEVEL_DEBUG, message, ##__VA_ARGS__);
+#else
+#define WTRACE(message, ...)
+#endif
